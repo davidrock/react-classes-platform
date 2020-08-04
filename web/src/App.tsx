558685c8a21e120abecbbcd16e9bab32e67cab4f
davidrock <david.martinelli@picpay.com>
2020-08-04 15:20:28 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Wolrd</h1>
+    </div>
+  );
+}
+
+export default App;
