c5cafa5eeaaeea846d6c6cd45c97b99c3ad54090
davidrock <david.martinelli@picpay.com>
2020-08-06 13:48:34 -0300

    Create responsive styles for Teacher List

--- web/src/pages/teacherList/index.tsx
+++ web/src/pages/teacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PageHeader from "../../components/page-header";
+import whtsIcon from "../../assets/images/icons/whatsapp.svg";
 import "./styles.css";
 
 function TeacherList() {
@@ -21,7 +22,38 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
-      <main></main>
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+              alt="avatar"
+            />
+            <div>
+              <strong>Diogo Gravatar</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br />
+            <br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das
+            pessoas através de experiências. Mais de 200.000 pessoas já passaram
+            por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>Preço por hora</p>
+            <strong>R$80,00</strong>
+            <button type="button">
+              <img src={whtsIcon} alt="whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
