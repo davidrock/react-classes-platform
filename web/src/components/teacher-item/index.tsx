add00908bbf38df1f18a4401cca67efc24b876a2
David Almeida <david.martinelli@picpay.com>
2020-08-15 03:57:50 -0300

    Make Teacher Item component usable

--- web/src/components/teacher-item/index.tsx
+++ web/src/components/teacher-item/index.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import whtsIcon from "../../assets/images/icons/whatsapp.svg";
 import "./styles.css";
+import api from "../../services/api";
 
-export default function TeacherItem() {
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(evt: FormEvent) {
+    evt.preventDefault();
+
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -11,28 +34,28 @@ export default function TeacherItem() {
           alt="avatar"
         />
         <div>
-          <strong>Diogo Gravatar</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>Preço por hora</p>
-        <strong>R$80,00</strong>
-        <button type="button">
+        <strong>R${teacher.cost}</strong>
+        <a
+          // target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+          type="button"
+        >
           <img src={whtsIcon} alt="whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
+
+export default TeacherItem;
