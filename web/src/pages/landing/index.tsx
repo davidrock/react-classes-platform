4e44f5e9636c87d87fa6cf4975c0efd6aaaa6bed
David Almeida <david.martinelli@picpay.com>
2020-08-15 00:45:27 -0300

    Add axios and connect to api

--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 
-import './styles.css';
+import "./styles.css";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function Landing() {
+  const [totalConnections, settotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((res) => {
+      const { total } = res.data;
+      settotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -35,7 +45,8 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
