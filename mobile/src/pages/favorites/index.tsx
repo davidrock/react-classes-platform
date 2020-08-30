5b7e06e6f4cf10f3bd5748ba2af174cc274d54d2
David Almeida <david.martinelli@picpay.com>
2020-08-29 22:43:48 -0300

    Add tittle to pages in tabs

--- mobile/src/pages/favorites/index.tsx
+++ mobile/src/pages/favorites/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { View } from "react-native";
 
 import styles from "./styles";
+import PageHeader from '../../components/page-header/index';
 
 export default function Favorites() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+    </View>
+  );
 }
--- mobile/src/pages/teacher-list/index.tsx
+++ mobile/src/pages/teacher-list/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
+import PageHeader from "../../components/page-header";
 import styles from "./styles";
 
 export default function TeacherList() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
+  );
 }
